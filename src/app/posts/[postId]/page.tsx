f915043a1bc9822a31a10fbd188c2abed468254c
Ant0yne <antoine.soliv@gmail.com>
2024-05-23 16:33:26 +0200

    post params

--- src/app/posts/[postId]/page.tsx
+++ src/app/posts/[postId]/page.tsx
@@ -1,11 +1,11 @@
-const Post = async () => {
-	const response = await fetch("https://dummyjson.com/posts?limit=10");
-	const data = await response.json();
+const Post = async ({ params }: { params: { postId: string } }) => {
+	const response = await fetch(`https://dummyjson.com/posts/${params.postId}`);
+	const post = await response.json();
 
 	return (
-		<main>
-			<h1></h1>
-			<p></p>
+		<main className="px-7 pt-24 text-center">
+			<h1 className="text-5xl font-semibold mb-7">{post.title}</h1>
+			<p className="max-w-[700px] mx-auto">{post.body}</p>
 		</main>
 	);
 };
