3248d9c79c188fef2e0e35532cc8d8b1b929c01f
Ant0yne <antoine.soliv@gmail.com>
2024-05-24 15:21:42 +0200

    server action

---
+++ src/app/create-post/page.tsx
@@ -0,0 +1,25 @@
+import { createPost } from "@/actions/actions";
+import React from "react";
+
+const CreatePost = () => {
+	return (
+		<main className="text-center pt-16 px-5">
+			<h1 className="text-4xl md:text-5xl font-bold mb-5">Create Post</h1>
+			<form action={createPost} className="h-10 space-x-2 mt-10">
+				<input
+					className="border rounded px-3"
+					type="text"
+					name="title"
+					id="title"
+					placeholder="Title for new post"
+					required
+				/>
+				<button className="h-full bg-blue-500 px-5 rounded text-white">
+					Submit
+				</button>
+			</form>
+		</main>
+	);
+};
+
+export default CreatePost;
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,6 +7,7 @@ import { usePathname } from "next/navigation";
 const navLinks = [
 	{ href: "/", label: "Home" },
 	{ href: "/posts", label: "Posts" },
+	{ href: "/create-post", label: "Create Post" },
 ];
 
 const Header = () => {
