91bf03c3ace091194caac10c0c3932a7eec0e2d2
Ant0yne <antoine.soliv@gmail.com>
2024-05-24 17:27:30 +0200

    authentication check

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,10 +1,18 @@
 "use server";
-// cSpell: words prisma
+// cSpell: words prisma, kinde
 
 import prisma from "@/lib/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 export const createPost = async (formData: FormData) => {
+	const { isAuthenticated } = getKindeServerSession();
+
+	if (!isAuthenticated()) {
+		redirect("api/auth/login?post_login_redirect_url=/create-post");
+	}
+
 	const title = formData.get("title") as string;
 	const body = formData.get("body") as string;
 
